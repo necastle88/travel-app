da5a5bb1205ca3d45374fbe3168a34de811f81c4
necastle88 <devLifeNC@gmail.com>
2021-04-08 22:53:23 -0700

    Style: creating form styles

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,40 +1,66 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   entry: "/src/client/index.js",
   output: {
-    path: path.resolve(process.cwd(), 'dist'),
-    libraryTarget: 'var',
-    library: 'Client'
+    path: path.resolve(process.cwd(), "dist"),
+    libraryTarget: "var",
+    library: "Client",
   },
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devtool: "source-map",
   module: {
     rules: [
       {
-        test: '/\.js$/',
+        test: "/.js$/",
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel-loader",
       },
       {
         test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset",
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-        template: "./src/client/views/index.html",
-        filename: "./index.html",
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
     }),
     new CleanWebpackPlugin({
       dry: true,
       verbose: true,
       cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false
-  })
-  ]
-}
+      protectWebpackAssets: false,
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
+          [
+            "svgo",
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+};
